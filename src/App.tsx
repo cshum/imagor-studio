b0b7d8befed965f644f0dbfc94fe42bd54d8ec9c
Adrian Shum <adrian@cshum.com>
2024-08-27 00:02:30 +0800

    add dropdown menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
 import './App.css'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from '@/components/ui/button.tsx';
 
 export default function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+  return <div>
+    <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    <div>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Button>Open</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  </div>
 }
