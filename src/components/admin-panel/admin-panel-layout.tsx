d7a88efa12da52fb49983a2a85d96e77bfbe8390
Adrian Shum <adrian@cshum.com>
2024-09-05 00:16:00 +0800

    shadcn ui sidebar progress

--- src/components/admin-panel/admin-panel-layout.tsx
+++ src/components/admin-panel/admin-panel-layout.tsx
@@ -1,16 +1,13 @@
 "use client";
 
-import { cn } from "@/lib/utils";
-import { useStore } from "@/hooks/use-store";
-import { Footer } from "@/components/admin-panel/footer";
-import { Sidebar } from "@/components/admin-panel/sidebar";
-import { useSidebarToggle } from "@/hooks/use-sidebar-toggle";
+import { cn } from '@/lib/utils';
+import { useStore } from '@/hooks/use-store';
+import { Footer } from '@/components/admin-panel/footer';
+import { Sidebar } from '@/components/admin-panel/sidebar';
+import { useSidebarToggle } from '@/hooks/use-sidebar-toggle';
+import { PropsWithChildren } from 'react';
 
-export default function AdminPanelLayout({
-  children
-}: {
-  children: React.ReactNode;
-}) {
+export default function AdminPanelLayout({ children }: PropsWithChildren) {
   const sidebar = useStore(useSidebarToggle, (state) => state);
 
   if (!sidebar) return null;
@@ -20,16 +17,16 @@ export default function AdminPanelLayout({
       <Sidebar />
       <main
         className={cn(
-          "min-h-[calc(100vh_-_56px)] bg-zinc-50 dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300",
-          sidebar?.isOpen === false ? "lg:ml-[90px]" : "lg:ml-72"
+          'min-h-[calc(100vh_-_56px)] bg-zinc-50 dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300',
+          sidebar?.isOpen === false ? 'lg:ml-[90px]' : 'lg:ml-72',
         )}
       >
         {children}
       </main>
       <footer
         className={cn(
-          "transition-[margin-left] ease-in-out duration-300",
-          sidebar?.isOpen === false ? "lg:ml-[90px]" : "lg:ml-72"
+          'transition-[margin-left] ease-in-out duration-300',
+          sidebar?.isOpen === false ? 'lg:ml-[90px]' : 'lg:ml-72',
         )}
       >
         <Footer />
--- src/layouts/demo-layout.tsx
+++ src/layouts/demo-layout.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren } from 'react';
 import AdminPanelLayout from '@/components/admin-panel/admin-panel-layout';
 
-export default function DemoLayout({ children }: PropsWithChildren<{}>) {
+export default function DemoLayout({ children }: PropsWithChildren) {
   return <AdminPanelLayout>{children}</AdminPanelLayout>;
 }
