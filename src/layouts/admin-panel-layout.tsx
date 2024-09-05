5e87fd99ae0918f140b8274043e63d40530f61f0
Adrian Shum <adrian@cshum.com>
2024-09-05 22:33:05 +0800

    cleanup

--- src/layouts/admin-panel-layout.tsx
+++ src/layouts/admin-panel-layout.tsx
@@ -5,7 +5,7 @@ import { PropsWithChildren } from 'react';
 import { useSidebarToggle } from '@/providers/sidebar-toggle-provider.tsx'
 
 export default function AdminPanelLayout({ children }: PropsWithChildren) {
-  const { isOpen } = useSidebarToggle(); // Use the new context-based hook
+  const { isOpen } = useSidebarToggle();
 
   return (
     <>
@@ -13,7 +13,7 @@ export default function AdminPanelLayout({ children }: PropsWithChildren) {
       <main
         className={cn(
           'min-h-[calc(100vh_-_56px)] bg-zinc-50 dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300',
-          isOpen === false ? 'lg:ml-[90px]' : 'lg:ml-72'
+          !isOpen ? 'lg:ml-[90px]' : 'lg:ml-72'
         )}
       >
         {children}
@@ -21,7 +21,7 @@ export default function AdminPanelLayout({ children }: PropsWithChildren) {
       <footer
         className={cn(
           'transition-[margin-left] ease-in-out duration-300',
-          isOpen === false ? 'lg:ml-[90px]' : 'lg:ml-72'
+          !isOpen ? 'lg:ml-[90px]' : 'lg:ml-72'
         )}
       >
         <Footer />
