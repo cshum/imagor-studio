5e0d13584f595d5cd45e19b3d12f99ddd012781d
Adrian Shum <adrian@cshum.com>
2025-07-24 14:25:03 +0800

    npm install

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
