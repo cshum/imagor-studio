cbdd7fa41100a1b1c8bb6a9193307c19f11fd923
Adrian Shum <adrian@cshum.com>
2025-09-15 13:23:48 +0800

    feat: use embed fs for migrations

--- server/internal/migrations/migrations.go
+++ server/internal/migrations/migrations.go
@@ -1,13 +1,18 @@
 package migrations
 
 import (
+	"embed"
+
 	"github.com/uptrace/bun/migrate"
 )
+
+//go:embed *.go
+var migrationsFS embed.FS
 
 var Migrations = migrate.NewMigrations()
 
 func init() {
-	if err := Migrations.DiscoverCaller(); err != nil {
+	if err := Migrations.Discover(migrationsFS); err != nil {
 		panic(err)
 	}
 }
