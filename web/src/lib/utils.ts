85f9620dab33f7d736eae30e1e0069c1421d0dbc
Adrian Shum <adrian@cshum.com>
2025-09-16 02:43:20 +0800

    cleanup

--- web/src/lib/utils.ts
+++ web/src/lib/utils.ts
@@ -11,15 +11,14 @@ export function cn(...inputs: ClassValue[]) {
  */
 export function debounce<T extends (...args: any[]) => any>(
   func: T,
-  wait: number
+  wait: number,
 ): (...args: Parameters<T>) => void {
   let timeout: NodeJS.Timeout | null = null
-  
+
   return (...args: Parameters<T>) => {
     if (timeout) {
       clearTimeout(timeout)
     }
-    
     timeout = setTimeout(() => {
       func(...args)
     }, wait)
