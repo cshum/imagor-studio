7d85b1ed0c33b847cc47b1a9676633c4d4f834f1
Adrian Shum <adrian@cshum.com>
2026-04-19 12:08:59 +0800

    refactor: module split

--- cloud/web/vite.config.ts
+++ cloud/web/vite.config.ts
@@ -1,23 +1,23 @@
-import path from 'path'
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, '../../web/src'),
-      '@cloud': path.resolve(__dirname, '../../web/src/cloud'),
-      '@shared': path.resolve(__dirname, '../../shared/web/src'),
+      "@": path.resolve(__dirname, "../../web/src"),
+      "@cloud": path.resolve(__dirname, "../../web/src/cloud"),
+      "@shared": path.resolve(__dirname, "../../shared/web/src"),
     },
   },
-  envDir: '../../web',
-  envPrefix: 'VITE_',
+  envDir: "../../web",
+  envPrefix: "VITE_",
   define: {
-    'import.meta.env.VITE_PRODUCT_MODE': JSON.stringify('cloud'),
+    "import.meta.env.VITE_PRODUCT_MODE": JSON.stringify("cloud"),
   },
   build: {
-    outDir: '../../server/static-cloud',
+    outDir: "../../server/static-cloud",
     emptyOutDir: true,
   },
-})
+});
